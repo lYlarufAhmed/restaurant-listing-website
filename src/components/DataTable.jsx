07058b59c8493281786d59233f05c8d6e875e381
maruf-ahmed <ahmedmaruf474@gmail.com>
2021-06-24 11:11:52 +0600

    search and filter are functional

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import React, {useRef, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {FlexContainer} from "./StyledComponents";
 
 
@@ -21,14 +21,19 @@ align-items: center;
 const PaginationControl = styled.button`
 height: 2rem;
 `
+
+function getFilters(data) {
+    let filters = []
+    new Set(data.reduce((acc, row) => acc.concat(row.genre.split(',')), [])
+        .sort((a, b) => ('' + a).localeCompare(('' + b)))
+    ).forEach((elem) => filters.push(elem))
+    return filters
+}
+
 export default function DataTable({data}) {
     let searchRef = useRef()
-    const DATA = data.sort(((a, b) => ('' + a.name).localeCompare(b.name)))
-    let genreChoices = []
-    new Set(
-        DATA.reduce((acc, row) => acc.concat(row.genre.split(',')), [])
-            .sort((a, b) => ('' + a).localeCompare(('' + b)))
-    ).forEach((elem) => genreChoices.push(elem))
+    // const DATA = data.sort(((a, b) => ('' + a.name).localeCompare(b.name)))
+
     let [state, setState] = useState({
         start: 0,
         end: 10,
@@ -36,7 +41,10 @@ export default function DataTable({data}) {
         filter: 'All',
         feedback: '',
         query: '',
-        totalPage: Math.ceil(DATA.length / 10)
+        totalPage: 1,
+        dataRows: data.sort(((a, b) => ('' + a.name).localeCompare(b.name))),
+        genreChoices: getFilters(data),
+        filteredData: []
     })
     let nextPage = () => {
         setState(prev => {
@@ -54,22 +62,57 @@ export default function DataTable({data}) {
             setState(prev => {
                 prev.currPage--
                 let [start, end] = [(prev.currPage - 1) * 10, prev.currPage * 10]
-                return {...prev, start, end}
+                // return {...prev, start, end}
+                prev.start = start
+                prev.end = end
+                return JSON.parse(JSON.stringify(prev))
             })
 
         }
     }
 
+    useEffect(() => {
+
+        // setState(prev => {
+        //     prev.totalPage = Math.ceil(prev.dataRows.length / 10)
+        //     prev.genreChoices = getFilters(prev.dataRows)
+        //     prev.start = 0
+        //     prev.end = 10
+        //     return JSON.parse(JSON.stringify(prev))
+        // })
+    }, [])
+
 
     function applyFilter(e) {
-        setState(prev => ({...prev, filter: e.target.value}))
+        console.log('use effect called')
+        setState(prev => {
+            let val = e.target.value
+            prev.filter = val
+            prev.start = 0
+            prev.end = 10
+            prev.filteredData = prev.dataRows.filter((row) => row.genre.includes(val))
+            prev.totalPage = Math.ceil(prev.filteredData.length / 10)
+            return JSON.parse(JSON.stringify(prev))
+        })
     }
 
     function filterBySearch() {
         let query = searchRef.current.value
         if (query.length) {
             if (query.length > 3) {
-                setState(prev => ({...prev, query, feedback: ''}))
+                setState(prev => {
+                    prev.query = query
+                    let searchResult = data.filter(row => row.name.includes(query) || row.city.includes(query))
+                    prev.dataRows = searchResult
+                    prev.feedback = searchResult.length ? `${searchResult.length} matches found` : 'No matches found'
+                    prev.currPage = 1
+                    prev.totalPage = Math.ceil(searchResult.length / 10)
+                    prev.start = 0
+                    prev.end = 10
+                    prev.filteredData = prev.filter !== 'All' ? searchResult.filter(row => row.genre.includes(prev.filter)) : []
+                    prev.genreChoices = getFilters(prev.dataRows)
+                    return JSON.parse(JSON.stringify(prev))
+                })
             } else {
                 setState(prev => ({...prev, feedback: 'Query should be at least 4 character long.'}))
             }
@@ -89,7 +132,16 @@ export default function DataTable({data}) {
                            if (e.key === 'Enter') filterBySearch()
                        }}
                        onInput={e => {
-                           if (!e.target.value.length) setState(prev => ({...prev, query: '', feedback: ''}))
+                           if (!e.target.value.length) setState(prev => {
+                               prev.dataRows = data
+                               prev.query = ''
+                               prev.feedback = ''
+                               // prev.filter = 'All'
+                               prev.filteredData = prev.filter === 'All' ? [] : data.filter(row => row.genre.includes(prev.filter))
+                               prev.totalPage = Math.ceil((prev.filteredData.length ? prev.filteredData.length : prev.dataRows.length) / 10)
+                               prev.genreChoices = getFilters(data)
+                               return JSON.parse(JSON.stringify(prev))
+                           })
                        }}
                 />
             </div>
@@ -105,8 +157,8 @@ export default function DataTable({data}) {
                     <th>phone number</th>
                     <th><span>
                         <label htmlFor={'genres'}>genres</label>
-                        <select name={'genres'} id={'genres'} onInput={(e) => applyFilter(e)}>
-                            {['All'].concat(genreChoices).map((g, index) => (
+                        <select name={'genres'} value={state.filter} id={'genres'} onInput={(e) => applyFilter(e)}>
+                            {['All'].concat(state.genreChoices).map((g, index) => (
                                 <option key={index} value={g}>{g}</option>
                             ))}
                         </select>
@@ -114,36 +166,34 @@ export default function DataTable({data}) {
                 </tr>
                 </thead>
                 <tbody>
-                {DATA
-                    .filter((row) => state.query ? (
-                        row.name.includes(state.query)
-                        ||
-                        row.city.includes(state.query)
-                        ||
-                        row.genre.includes(state.query)
-                    ) : true)
+                {state.filteredData.length ? state.filteredData
                     .slice(state.start, state.end)
-                    .filter((row) => state.filter === 'All' ? true : row.genre.split(',').some((g) => g === state.filter))
-                    .map((row, index) => {
-                        return (
-                            <tr key={index}>
-                                <td>{index + 1}</td>
-                                <td>{row.name}</td>
-                                <td>{row.city}</td>
-                                <td>{row.state}</td>
-                                <td>{row.telephone}</td>
-                                <td>{row.genre}</td>
-                            </tr>
-                        )
-                    })}
+                    .map((row, index) => <TableRow key={index} row={row} index={index}/>) : state.dataRows
+                    .slice(state.start, state.end)
+                    .map((row, index) => <TableRow key={index} row={row} index={index}/>)}
                 </tbody>
 
             </table>
             <Pagination>
+                <p>Total pages: {state.totalPage}</p>
                 <PaginationControl onClick={() => prevPage()}>prev</PaginationControl>
                 {state.currPage}
                 <PaginationControl onClick={() => nextPage()}>next</PaginationControl>
             </Pagination>
         </Wrapper>
+    )
+}
+
+
+function TableRow({row, index}) {
+    return (
+        <tr key={index}>
+            <td>{index + 1}</td>
+            <td>{row.name}</td>
+            <td>{row.city}</td>
+            <td>{row.state}</td>
+            <td>{row.telephone}</td>
+            <td>{row.genre}</td>
+        </tr>
     )
 }
